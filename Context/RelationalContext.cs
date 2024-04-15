f5ec3da05043104157aaa2cb8c80e5a2e8957f7d
dimaskvitka <dimaskvitka@gmail.com>
2024-04-15 23:33:32 +0300

    Add project files.

---
+++ Context/RelationalContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace EnergyEfficiencyBE.Context
+{
+    public class RelationalContext : DbContext
+    {
+        public RelationalContext(DbContextOptions<RelationalContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+using EnergyEfficiencyBE.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<RelationalContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Relational")));
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
